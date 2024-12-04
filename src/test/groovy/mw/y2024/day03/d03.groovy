0b9959df8b844c6b6097693fcb0344303d39922b
Martin Wangel <martin.wangel@wirelesscar.com>
2024-12-04 09:02:32 +0100

    small version of 3 for a forum post

--- src/test/groovy/mw/y2024/day03/d03.groovy
+++ src/test/groovy/mw/y2024/day03/d03.groovy
@@ -6,7 +6,7 @@ import spock.lang.Specification
 class d03 extends Specification {
 
   def "Day 03"() {
-    given: input = this.getClass().getResource( '/y2024/data03.txt' ).readLines()
+    given: def input = this.getClass().getResource( '/y2024/data03.txt' ).readLines().join()
 
     when: def c = load(input)
     then: c.collect { Mul mul -> mul.execute().toLong() }.sum() == 187825547
@@ -15,9 +15,19 @@ class d03 extends Specification {
     then: d.collect { Mul mul -> mul.execute().toLong() }.sum() == 85508223
   }
 
+  def "2024-04 in Groovy as a Spock test case"() {
+    given:
+      def text = this.getClass().getResource( '/y2024/data03.txt' ).readLines().join()
+      def ops = text.findAll(/mul\((\d+),(\d+)\)/)
+      def res = ops.sum {
+        def parts = it[4..-2].split(",").collect { it.toLong() }
+        parts[0] * parts[1]
+      }
+    expect: res == 187825547
+  }
 
-  def load( List<String> lines ) {
-    def s = lines.join().findAll(/mul\((\d+),(\d+)\)/)
+  def load( String text ) {
+    def s = text.findAll(/mul\((\d+),(\d+)\)/)
 
     def muls = s.collect { String line ->
       Mul.parse( line )
@@ -27,8 +37,8 @@ class d03 extends Specification {
   }
 
 
-  def load2( List<String> lines ) {
-    def hits = lines.join().findAll(/mul\((\d+),(\d+)\)|do\(\)|don't\(\)/)
+  def load2( String text ) {
+    def hits = text.findAll(/mul\((\d+),(\d+)\)|do\(\)|don't\(\)/)
 
     boolean doIt = true
     def muls = []
