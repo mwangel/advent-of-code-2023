af9ad20d8fec44ae6af6695e3e44022383422466
Martin Wangel <martin.wangel@wirelesscar.com>
2024-12-09 13:38:58 +0100

    2024 8 part 1 done

--- src/test/groovy/mw/y2024/day08/d08.groovy
+++ src/test/groovy/mw/y2024/day08/d08.groovy
@@ -6,19 +6,67 @@ class d08 extends Specification {
 
   def "Day 08"() {
     given:
-      def( fileName, expectedResult1, expectedResult2 ) = [ '/y2024/data08-test.txt', 41, 6 ]
-//      def( fileName, expectedResult1, expectedResult2 ) = [ '/y2024/data08.txt', 4977, 1729 ]
+//      def( fileName, w1, h1, expectedResult1, expectedResult2 ) = [ '/y2024/data08-test.txt', 12, 12, 14, 0 ]
+      def( fileName, w1, h1, expectedResult1, expectedResult2 ) = [ '/y2024/data08.txt', 50, 50, 354, 0 ]
 
       def input = this.getClass().getResource( fileName ).readLines()
       def (map, width, height) = loadInput( input )
 
     when:
-      def result = 0
-      println(map)
+      println(map + "\n---")
+      def result = calculateAntinodePositions( map, width, height )
+      println(result)
+      paint( map, result, width, height )
     then:
-      width == 12
-      height == 12
-      result == expectedResult1
+      width == w1
+      height == h1
+      result.size() == expectedResult1
+  }
+
+
+  def calculateAntinodePositions( Map<Character,List<Tuple2<Long,Long>>> map, int width, int height ) {
+    def antinodePositions = [] as List<Tuple2<Long,Long>>
+
+    map.each { Character c, List<Tuple2<Long,Long>> positions ->
+      for( int a=0; a < positions.size(); a++ ) {
+        for( int b=a+1; b < positions.size(); b++ ) {
+          Tuple2<Long,Long> p1 = positions[a]
+          Tuple2<Long,Long> p2 = positions[b]
+          antinodePositions.addAll( calculateAntinodes( p1, p2 ) )
+        }
+      }
+    }
+    return antinodePositions.unique().findAll { Tuple2<Long,Long> p -> p.V1 >= 0 && p.V2 >= 0 && p.V1 < width && p.V2 < height }
+  }
+
+  def paint( Map<Character,List> input, List<Tuple2<Long,Long>> antinodePositions, int width, int height ) {
+    // print the map with input and antinode positions
+    for( int y=0; y < height; y++ ) {
+      for( int x=0; x < width; x++ ) {
+        def inlist = input.find { it.value.contains( [x, y] ) }
+        def c = inlist ? inlist.key : '.'
+
+        if( antinodePositions.contains( [x, y] ) ) {
+          print( "#" )
+        }
+        else {
+          print( c )
+        }
+      }
+      println()
+    }
+  }
+
+
+  def calculateAntinodes( Tuple2<Long,Long> p1, Tuple2<Long,Long> p2 ) {
+    def dx = p2.V1 - p1.V1
+    def dy = p2.V2 - p1.V2
+    def antinodes = [] as List<Tuple2<Long,Long>>
+
+    antinodes << new Tuple2<Long,Long>(p1.first - dx, p1.second - dy)
+    antinodes << new Tuple2<Long,Long>(p2.first + dx, p2.second + dy)
+
+    return antinodes
   }
 
 
