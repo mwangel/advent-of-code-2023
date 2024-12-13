6076c2334348a785b1acfba3b273c6e63a693c6c
Martin Wangel <martin.wangel@wirelesscar.com>
2024-12-13 15:28:43 +0100

    2024 day 11 parts 1 and 2

--- src/test/groovy/mw/y2024/day11/d11.groovy
+++ src/test/groovy/mw/y2024/day11/d11.groovy
@@ -7,76 +7,67 @@ import spock.lang.Unroll
 class d11 extends Specification {
 
   @Unroll
-  def "Day 11 part 1"() {
-    when:
-      def result = part1( input, blinks )
+  def "Using non-recursive rundown returns #expected for #blinks blinks and input #input"() {
+    given:
+      // We can actually find the results for all numbers in the input and store them in this map.
+      Map<Long,List<Long>> knownChildNumbers = [:].withDefault { [] as List<Long> }
+      Map<String,Long> knownCounts = [:]
 
-    then:
-      long startTime = System.currentTimeMillis()
-      result == expected
-      println "Time: ${System.currentTimeMillis() - startTime} ms"
-
-    where:
-      expected || blinks | input
-      55312    || 25     | [125, 17] as LinkedList<Long>
-      212655   || 25     | [4, 4841539, 66, 5279, 49207, 134, 609568, 0] as LinkedList<Long>
-  }
-
-
-  def "something else"() {
-    when:
-      Map<Long,Set<Long>> known = [:].withDefault { [] as Set<Long> }
-      known[0L] << 1L
-
-      //def input = [125, 17] as LinkedList<Long> // This results in us knowing the results of 76 numbers.
-      def input = [4, 4841539, 66, 5279, 49207, 134, 609568, 0] as LinkedList<Long> // This results in us knowing the results of ? numbers.
-
-      input.each { long nr ->
-        recursiveRundown( nr, known )
-      }
-      println("Now we know the results of ${known.size()} numbers.")
-
-    then:
-      false
-  }
-
-  def "Using non-recursive rundown"() {
-    when:
-      Map<Long,Set<Long>> known = [:].withDefault { [] as Set<Long> }
-      known[0L] << 1L
-
-      def input = [125, 17] as LinkedList<Long> // This results in us knowing the results of 76 numbers.
-      input = [4, 4841539, 66, 5279, 49207, 134, 609568, 0] as LinkedList<Long> // This results in us knowing the results of ? numbers.
-
-      def unknowns = [] as Set<Long>
-      unknowns.addAll( input )
+      def unknowns = new HashSet<Long>( input )
 
       while( unknowns.size() ) {
-        def nr = unknowns.first()
+        long nr = unknowns.first()
         def result = nonrecursiveRundown( nr )
-        known[nr].addAll( result )
+        knownChildNumbers[nr].addAll( result )
         unknowns.remove( nr )
         result.each { long n ->
-          if( !known.containsKey( n ) ) {
+          if( !knownChildNumbers.containsKey( n ) ) {
             unknowns << n
           }
         }
       }
-      println("Now we know the results of ${known.size()} numbers.")
+      println("Now we know the results of ${knownChildNumbers.size()} numbers. Try blinking $blinks times.")
 
-      long childCount = 0
-      input.each { long nr ->
-        childCount += countChildren( nr, known )
-      }
+    when:
+      def childCount = input.sum { long nr -> countChildrenRecursively( nr, knownChildNumbers, knownCounts, blinks ) }
 
     then:
-      known.size() == 3960
-      childCount == 1
+      childCount == expected
+
+    where:
+      expected        || blinks | input
+      3               || 1      | [125L, 17L] as LinkedList<Long>
+      5               || 3      | [125L, 17L] as LinkedList<Long>
+      9               || 4      | [125L, 17L] as LinkedList<Long>
+      22              || 6      | [125L, 17L] as LinkedList<Long> // given in the puzzle
+      31              || 7      | [125L, 17L] as LinkedList<Long>
+      55312           || 25     | [125L, 17L] as LinkedList<Long> // given in the puzzle
+      65601038650482  || 75     | [125L, 17L] as LinkedList<Long>
+      11              || 1      | [4L, 4841539L, 66L, 5279L, 49207L, 134L, 609568L, 0L] as LinkedList<Long>
+      212655          || 25     | [4L, 4841539L, 66L, 5279L, 49207L, 134L, 609568L, 0L] as LinkedList<Long> // Part 1
+      253582809724830 || 75     | [4L, 4841539L, 66L, 5279L, 49207L, 134L, 609568L, 0L] as LinkedList<Long> // Part 2
+  }
+
+
+  @Unroll
+  def "Non-recursive rundown for #nr returns #expected"() {
+    expect:
+      nonrecursiveRundown( nr ) == expected
+
+    where:
+      nr     || expected
+      0      || [1L]
+      1      || [2024]
+      11     || [1, 1]
+      22     || [2, 2]
+      123456 || [123, 456]
   }
 
 
   // Find the result (as a List) of "nr" by applying the rules of the game.
   def nonrecursiveRundown( long nr ) {
+    if( nr == 0 ) return [1]
+
     String s = nr.toString()
     int x = s.size()
     if( x % 2 == 0 ) {
@@ -86,71 +77,40 @@ class d11 extends Specification {
       return [n1, n2]
     }
 
-    return [nr * 2024]
-  }
-
-  // Starting on "nr" recursively find all the children and count them.
-  def countChildren( long nr, Map<Long, Set<Long>> known ) {
-    def results = known[nr]
-    if( results.size() == 0 ) {
-      return 0
-    }
-    else {
-      return known[nr].sum { long n ->
-        countChildren( n, known )
-      }
-    }
-  }
-
-
-  def recursiveRundown( long nr, Map<Long,Set<Long>> known ) {
-    if( known.containsKey( nr ) )
-      return known[nr]
-
-    String s = nr.toString()
-    int x = s.size()
-    if( x % 2 == 0 ) {
-      int xx = x >> 1
-      //Set<Long> result = new HashSet<>()
-      long n1 = s[0..xx-1].toLong()
-      long n2 = s[xx..x-1].toLong()
-      known[nr] << n1
-      known[nr] << n2
-      recursiveRundown( n1, known )
-      recursiveRundown( n2, known )
-      return known[nr]
-    }
-
-    known[nr] << nr * 2024
-    recursiveRundown( nr * 2024, known )
-    return known[nr]
+    return [nr * 2024L]
   }
 
 
   /**
-   * This is the most straight-forward solution to part 1.
-   * Unfortunately it will run out of memory when the number of blinks is too high.
-   **/
+   * Starting on "nr" recursively find all the children and count them.
+   * @param nr The number to start from.
+   * @param knownChildren A map of known child NUMBERS for each number, eg. [ 0:[1], 1234:[12,34] ]
+   * @param knownCounts A map of known final child COUNTS for each number at each level, eg. [ "0;1":1, "1234;1":2 ]
+   * @param level The number of levels to go down from here. Decreases by 1 for each recursion so start with 75 if you want to blink 75 times.
+   */
   @CompileStatic
-  long part1( LinkedList<Long> input, int blinks ) {
-    blinks.times { int blink ->
-      def nextGen = [] as LinkedList<Long>
-      input.each { Long nr ->
-        String s = nr.toString()
-        int x = s.size()
-
-        if( nr == 0 ) nextGen << 1L
-        else if( x % 2 == 0 ) {
-          int xx = x >> 1
-          nextGen << s[0..xx-1].toLong()
-          nextGen << s[xx..x-1].toLong()
-        }
-        else nextGen << nr * 2024
-      }
-      println( " $blink: $input -> $nextGen\n" )
-      input = nextGen
+  long countChildrenRecursively( long nr, Map<Long, List<Long>> knownChildren, Map<String,Long> knownCounts, int level ) {
+    // We know the result for this number at this level.
+    String key = "$nr;$level"
+    if( knownCounts.containsKey( key ) ) {
+      return knownCounts[key]
     }
 
-    return input.size()
+    def childNumbers = knownChildren[nr]
+    if( childNumbers.size() == 0 ) { throw new RuntimeException( "No children found for $nr (level = $level)" ) }
+
+    if( level == 1 ) {
+      knownCounts[key] = childNumbers.size().toLong()
+      return childNumbers.size()
+    }
+
+    long childCount = 0
+    childNumbers.each { long n ->
+      childCount += countChildrenRecursively( n, knownChildren, knownCounts, level - 1 )
+    }
+    knownCounts[key] = childCount
+
+    return childCount
   }
+
 }
