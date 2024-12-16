2f16cfc5f08e8d8afc7664e260fae4bc71c1b66e
Martin Wangel <martin.wangel@wirelesscar.com>
2024-12-16 15:40:08 +0100

    2024 13 part 1

--- src/test/groovy/mw/y2024/day13/d13.groovy
+++ src/test/groovy/mw/y2024/day13/d13.groovy
@@ -1,4 +1,4 @@
-  package mw.y2024.day13
+package mw.y2024.day13
 
 import spock.lang.Shared
 import spock.lang.Specification
@@ -8,8 +8,8 @@ class d13 extends Specification {
 
   def "Day 13 - integer division"() {
     given:
-      def( fileName, expected1 ) = [ '/y2024/data13-test.txt', 480 ]
-//      def( fileName, expected1 ) = [ '/y2024/data13.txt', 624 ]
+//      def (fileName, expected1) = ['/y2024/data13-test.txt', 480]
+      def( fileName, expected1 ) = [ '/y2024/data13.txt', 624 ]
       def input = this.getClass().getResource( fileName ).readLines()
 
     when:
@@ -26,7 +26,7 @@ class d13 extends Specification {
     def patternB = ~"Button B: X\\+(?<x>\\d+), Y\\+(?<y>\\d+)" // Button B: X+0, Y+0
     def patternP = ~"Prize: X=(?<x>\\d+), Y=(?<y>\\d+)" // Prize: X=0, Y=0
 
-    for( int index=0; index < data.size(); index++ ) {
+    for( int index = 0; index < data.size(); index++ ) {
       def line = data[index]
 
       int xStepA = 0
@@ -36,28 +36,63 @@ class d13 extends Specification {
       int targetX = 0
       int targetY = 0
 
-      if( line.isEmpty(  )) continue
+      if( line.isEmpty() ) continue
 
       def matcherA = line =~ patternA
       if( !matcherA.matches() ) throw new RuntimeException( "Unexpected line: $line" )
-      xStepA = matcherA.group("x") as int
-      yStepA = matcherA.group("y") as int
+      xStepA = matcherA.group( "x" ) as int
+      yStepA = matcherA.group( "y" ) as int
 
       line = data[++index]
       def matcherB = line =~ patternB
       if( !matcherB.matches() ) throw new RuntimeException( "Unexpected line: $line" )
-      xStepB = matcherB.group("x") as int
-      yStepB = matcherB.group("y") as int
+      xStepB = matcherB.group( "x" ) as int
+      yStepB = matcherB.group( "y" ) as int
 
       line = data[++index]
       def matcherP = line =~ patternP
       if( !matcherP.matches() ) throw new RuntimeException( "Unexpected line: $line" )
-      targetX = matcherP.group("x") as int
-      targetY = matcherP.group("y") as int
+      targetX = matcherP.group( "x" ) as int
+      targetY = matcherP.group( "y" ) as int
 
-      println( "A:$xStepA,$yStepA - B:$xStepB,$yStepB - P:$targetX,$targetY" )
+      //println( "A:$xStepA,$yStepA - B:$xStepB,$yStepB - P:$targetX,$targetY" )
+      def paths = findPaths( xStepA, yStepA, xStepB, yStepB, targetX, targetY )
+      if( paths ) {
+        def cheapest = findCheapestPath( paths )
+        def cost = 3 * cheapest[0] + cheapest[1]
+        println( paths )
+        println( "Cheapest is ${cheapest} : ${cost}" )
+        result += cost
+      }
+      else {
+        println( "No path found for $xStepA,$yStepA / $xStepB,$yStepB -> $targetX,$targetY " )
+      }
+      println( "----" )
     }
     return result
+  }
+
+
+  def findCheapestPath( List<List<Long>> paths ) {
+    def cheapest = paths.min { List<Long> p -> 3*p[0] + p[1] }
+    return cheapest
+  }
+
+
+  def findPaths( int xa, int ya, int xb, int yb, int xp, int yp ) {
+    def paths = []
+
+    for( int i = 0; i < 100; i++ ) {
+      def x = i * xa
+      def y = i * ya
+      if( (xp - x) % xb == 0 && (yp - y) % yb == 0 ) {
+        def j = (xp - x) / xb
+        if( j == (yp - y) / yb ) {
+          paths.add( [i.toLong(), j.toLong()] )
+        }
+      }
+    }
+    return paths
   }
 
 }
